6b12d1a9fa3948ab70f31fc028684c4161171a07
Rika Sonohara <rika.s8778@gmail.com>
2024-04-16 17:46:06 -0700

    changed default values from daily to weekly.

--- js/script.js
+++ js/script.js
@@ -13,7 +13,40 @@ let lastSelfCare = '';
 
 
 
-if(currentWork.length == 0 || document.getElementById('daily-link').onclick()){
+if(currentWork.length == 0 || document.getElementById('weekly-link').onclick()){
+    fetch('../data.json')
+    .then(res => {
+    return res.json();
+    })
+    .then(function(data1) {
+        currentWork = data1[0].timeframes.weekly.current + 'hrs';
+        lastWork = ' Last Week - ' +data1[0].timeframes.weekly.previous + 'hrs';
+        currentPlay = data1[1].timeframes.weekly.current + 'hrs';
+        lastPlay = ' Last Week - ' +data1[1].timeframes.weekly.previous + 'hrs';
+        currentStudy = data1[2].timeframes.weekly.current + 'hrs';
+        lastStudy = ' Last Week - ' +data1[2].timeframes.weekly.previous + 'hrs';
+        currentExercise = data1[3].timeframes.weekly.current + 'hrs';
+        lastExercise = ' Last Week - ' +data1[3].timeframes.weekly.previous + 'hrs';
+        currentSocial = data1[4].timeframes.weekly.current + 'hrs';
+        lastSocial = ' Last Week - ' +data1[4].timeframes.weekly.previous + 'hrs';
+        currentSelfCare = data1[5].timeframes.weekly.current + 'hrs';
+        lastSelfCare = ' Last Week - ' +data1[5].timeframes.weekly.previous + 'hrs';
+        document.getElementById('current-work').innerHTML = currentWork;
+        document.getElementById('last-work').innerHTML = lastWork;
+        document.getElementById('current-play').innerHTML = currentPlay;
+        document.getElementById('last-play').innerHTML = lastPlay;
+        document.getElementById('current-study').innerHTML = currentStudy;
+        document.getElementById('last-study').innerHTML = lastStudy;
+        document.getElementById('current-exercise').innerHTML = currentExercise;
+        document.getElementById('last-exercise').innerHTML = lastExercise;
+        document.getElementById('current-social').innerHTML = currentSocial;
+        document.getElementById('last-social').innerHTML = lastSocial;
+        document.getElementById('current-self-care').innerHTML = currentSelfCare;
+        document.getElementById('last-self-care').innerHTML = lastSelfCare;
+    })
+};
+
+function showDailyValues() {
     fetch('../data.json')
     .then(res => {
     return res.json();
@@ -44,9 +77,9 @@ if(currentWork.length == 0 || document.getElementById('daily-link').onclick()){
         document.getElementById('current-self-care').innerHTML = currentSelfCare;
         document.getElementById('last-self-care').innerHTML = lastSelfCare;
     })
-    
 };
 
+/* cnahged defaul values from daily to weekly
 function showWeeklyValues() {
     fetch('../data.json')
     .then(res1 => {
@@ -81,15 +114,14 @@ function showWeeklyValues() {
         document.getElementById('last-self-care').innerHTML = lastSelfCare;
     })
 };
+*/
 
 function showMonthlyValues() {
     fetch('../data.json')
     .then(res1 => {
-        //console.log(res1)
     return res1.json()
     })
     .then(function(data1) {
-        console.log();
         currentWork = data1[0].timeframes.monthly.current + 'hrs';
         lastWork = ' Last Week - ' +data1[0].timeframes.monthly.previous + 'hrs';
         currentPlay = data1[1].timeframes.monthly.current + 'hrs';
